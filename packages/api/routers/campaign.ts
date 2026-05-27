ab9ff1894ce068feb394bdd68a88ce27b8aad1b4
Terb-13 <44187596+Terb-13@users.noreply.github.com>
2026-05-27 10:32:55 -0400

    feat(customer): include artwork thumbnails in getMine for customer previews

--- packages/api/routers/campaign.ts
+++ packages/api/routers/campaign.ts
@@ -1,10 +1,5 @@
-import { inngest } from "../inngest/client";
-
-  // After creating/updating the artwork
-  await inngest.send({
-    name: "artwork/uploaded",
-    data: {
-      artworkId: artwork.id,
-      fileUrl: input.fileUrl,
-    },
-  });
+        artwork: {
+          include: {
+            thumbnails: true,
+          },
+        },
